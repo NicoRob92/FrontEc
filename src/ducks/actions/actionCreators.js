bacb3029284fa6c749869b3bd1e607b000c09ed7
LucasSantillan0 <lukitas6641@gmail.com>
2022-01-21 17:32:25 -0300

    nuevas funciones y estado de carga

--- src/ducks/actions/actionCreators.js
+++ src/ducks/actions/actionCreators.js
@@ -3,13 +3,26 @@ import * as actionTypes from "./actionTypes"
 const api ="http://localhost:4000/api/"
 
 const getProductsUrl = api + "products"
+const getPostsUrl = api + "Post"
 const getCategoriesUrl = api + "category"
 
 
 export function getProducts() {
     return function(dispatch) {
-      dispatch({ type: "LOADING", payload: "LOADING" });
+      dispatch({ type: "LOADING", payload: "products" });
       return fetch(getProductsUrl)
+          .then(response => response.json())
+          .then(json => {                   
+              dispatch({ type: actionTypes.GET_PRODUCTS, payload: json });                      
+          })
+          .catch(e=>console.error(e))
+    };
+}
+
+export function getProductsByName(name) {
+    return function(dispatch) {
+      dispatch({ type: "LOADING", payload: "products" });
+      return fetch(getProductsUrl+"?name="+name)
           .then(response => response.json())
           .then(json => {                   
               dispatch({ type: actionTypes.GET_PRODUCTS, payload: json });                      
@@ -20,7 +33,7 @@ export function getProducts() {
 
 export function getCategories (){
     return function(dispatch) {
-        dispatch({ type: "LOADING", payload: "LOADING" });
+        dispatch({ type: "LOADING", payload: "categories" });
         return fetch(getCategoriesUrl)
             .then(response => response.json())
             .then(json => {                   
@@ -29,6 +42,19 @@ export function getCategories (){
             .catch(e=>console.error(e))
       };
 }
+
+export function getPosts() {
+    return function(dispatch) {
+      dispatch({ type: "LOADING", payload: "posts" });
+      return fetch(getPostsUrl)
+          .then(response => response.json())
+          .then(json => {                   
+              dispatch({ type: actionTypes.GET_POSTS, payload: json });                      
+          })
+          .catch(e=>console.error(e))
+    };
+}
+
 
 export function getCategoryProducts(categoryId){
     return {type: actionTypes.GET_CATEGORY_PRODUCTS, payload:categoryId}
--- src/ducks/reducers/reducer.js
+++ src/ducks/reducers/reducer.js
@@ -3,7 +3,8 @@ import * as actionTypes from "../actions/actionTypes"
 const initialState = {
   products: [],
   categories: [],
-  categoryProducts:[]
+  categoryProducts:[],
+  posts: [],
 };
 
 export default function Product(state = initialState, action) {
@@ -12,14 +13,18 @@ export default function Product(state = initialState, action) {
       return { ...state, products: action.payload };
 
     case actionTypes.GET_CATEGORIES:
-        return { ...state, categories: action.payload };
+      return { ...state, categories: action.payload };
+
+    case actionTypes.GET_POSTS:
+      return { ...state, posts: action.payload };
 
     case actionTypes.GET_CATEGORY_PRODUCTS:
       return {...state, 
         categoryProducts:state.products
         .filter(product=>product.categoryId==action.payload)
       }
-
+    case "LOADING":
+      return {...state, [action.payload]:["loading"]}
     default:
       return state;
   }
