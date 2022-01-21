58a8cb52c9ab2a31ef2204694008ce8786a83530
c4chris124 <c4chris124@gmail.com>
2022-01-21 16:16:52 -0600

    Replace structure for class components, completed form structures and data, missing controlled form and dispatch action

--- src/components/FormNewPost/FormNewPost.js
+++ src/components/FormNewPost/FormNewPost.js
@@ -1,8 +1,11 @@
-import { Component, useState } from "react";
+import React, { Component } from "react";
 import FormProductDetail from './FormProductDetail'
 import FormProductDescription from "./FormProductDescription";
-const FormNewPost = () => {
-    const [state, setState] = useState({
+import Confirm from "./Confirm";
+import Success from "./Success";
+
+export default class FormNewPost extends Component {
+    state = {
         step: 1,
         title: '',
         category: '',
@@ -11,57 +14,53 @@ const FormNewPost = () => {
         photos: '',
         description: '',
         price: '',
-    })
+    }
 
     // Proceed to next step
-    const newStep = () => {
-        const { step } = state
-        setState({
+    nextStep = () => {
+        const { step } = this.state
+        this.setState({
             step: step + 1
         })
     }
 
     // Go to previous step
-    const prevStep = () => {
-        const { step } = state
-        setState({
+    prevStep = () => {
+        const { step } = this.state
+        this.setState({
             step: step - 1
         })
     }
 
     // Handle field changes
-    const handleChange = e => {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        })
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value})
     }
 
-    const { step } = state
-    const { category, title, condition, amount, photos, description, price } = state
-    const values = { category, title, condition, amount, photos, description, price }
-
-    switch (step) {
-        case 1:
-            return (
-                <FormProductDetail newStep={newStep} handleChange={handleChange} values={values} />
-            )
-        case 2:
-            return (
-                <FormProductDescription newStep={newStep} prevStep={prevStep} handleChange={handleChange} values={values} />
-            )
-        case 3:
-            return (
-                <h1>Confirm</h1>
-            )
-        case 4:
-            return (
-                <h1>Success</h1>
-            )
-        default:
-            break;
+    render() {
+        const { step } = this.state
+        const { title, category, condition, amount, photos, description, price } = this.state
+        const values = { title, category, condition, amount, photos, description, price}
+        switch (step) {
+            case 1:
+                return (
+                    <FormProductDetail nextStep={this.nextStep} handleChange={this.handleChange} values={values} />
+                )
+            case 2:
+                return (
+                    <FormProductDescription nextStep={this.nextStep} prevStep={this.prevStep} handleChange={this.handleChange} values={values} />
+                )
+            case 3:
+                return (
+                    <Confirm nextStep={this.nextStep} prevStep={this.prevStep} values={values}/>
+                )
+            case 4:
+                return (
+                    <Success/>
+                )
+            default:
+                break;
+        }
     }
-
 }
 
-export default FormNewPost;
