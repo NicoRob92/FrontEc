d447dc8763bf36850ef17fd1ee464d2b890c1d12
c4chris124 <c4chris124@gmail.com>
2022-01-24 19:55:24 -0600

    Chaging components to a functional components

--- src/components/FormNewPost/FormNewPost.js
+++ src/components/FormNewPost/FormNewPost.js
@@ -1,103 +1,88 @@
-import { Component } from "react"
+import { useState } from "react"
 import FormProductDetail from "./FormProductDetail"
 import FormProductDescription from "./FormProductDescription"
 import Confirm from "./Confirm"
 import Success from "./Success"
 import validate from "./Validation"
 
-const inititalValue = {
-    title: '',
-    category: '',
-    condition: '',
-    amount: '',
-    photos: '',
-    description: '',
-    price: '',
-}
+export default function FormNewPost () {
+    const [step, setStep] = useState(1)
+    const [input, setInput] = useState({
+        title: '',
+        categoryId: '',
+        condition: '',
+        stock: '',
+        images: '',
+        description: '',
+        price: '',
+    })
+    const [errors, setErrors] = useState({})
 
-export default class FormNewPost extends Component {
-
-    state = {
-        step: 1,
-        input: inititalValue,
-        errors: {}
-    }
-
-    handleChange = this.handleChange.bind(this)
-    handleBlur = this.handleBlur.bind(this)
 
 
     // Proceed to next step
-    nextStep = () => {
-        const { step } = this.state
-        this.setState({
-            step: step + 1
-        })
+    const nextStep = () => {
+        setStep(step + 1)
     }
 
     // Go to previous step
-    prevStep = () => {
-        const { step } = this.state
-        this.setState({
-            step: step - 1
-        })
+    const prevStep = () => {
+        setStep(step - 1)
     }
 
     // Handle field changes
-    handleChange(e) {
-        this.setState((state) => {
-            const { input } = state
+    function handleChange(e) {
+        setInput((input) => {
             const { name, value } = e.target
             return {
-                input: {
                     ...input,
                     [name]: value
-                }
             }
         })
     }
-
-    handleBlur() {
-        this.setState({
-            errors: validate(this.state.input)
-        })
+    // Handle errors by blur event
+    function handleBlur() {
+        setErrors(validate(input))
     }
 
-    render() {
-        const { step } = this.state
-        const { title, category, condition, amount, photos, description, price } = this.state.input
-        const values = { title, category, condition, amount, photos, description, price }
+    function handleSubmit(e){
+        e.preventDefault()
+
+    }
+
         switch (step) {
             case 1:
                 return (
                     <FormProductDetail
-                        nextStep={this.nextStep}
-                        handleChange={this.handleChange}
-                        handleBlur={this.handleBlur}
-                        values={values} />
+                        nextStep={nextStep}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        input={input}/>
                 )
             case 2:
                 return (
                     <FormProductDescription
-                        nextStep={this.nextStep}
-                        prevStep={this.prevStep}
-                        handleChange={this.handleChange}
-                        handleBlur={this.handleBlur}
-                        values={values} />
+                        nextStep={nextStep}
+                        prevStep={prevStep}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        input={input}
+                        />
                 )
             case 3:
                 return (
                     <Confirm
-                        nextStep={this.nextStep}
-                        prevStep={this.prevStep}
-                        values={values} />
+                        nextStep={nextStep}
+                        prevStep={prevStep}
+                        input={input}
+                        />
                 )
             case 4:
                 return (
                     <Success />
                 )
             default:
-                break;
+                return null
         }
-    }
+
 }
--- src/components/FormNewPost/FormProductDescription.js
+++ src/components/FormNewPost/FormProductDescription.js
@@ -1,57 +1,55 @@
-import { Component } from "react"
 
-export default class FormProductDescription extends Component {
-    continues = e => {
+const FormProductDescription = ({ nextStep, prevStep, handleChange, handleBlur, input }) => {
+    const continues = e => {
         e.preventDefault()
-        this.props.nextStep()
+        nextStep()
     }
 
-    back = e => {
+    const back = e => {
         e.preventDefault()
-        this.props.prevStep()
+        prevStep()
     }
-    render() {
-        const { values, handleBlur, handleChange } = this.props
         return (
             <form>
                 <div className="mb-3">
                     <label className="form-label">Choose Category</label>
-                    <select className="form-select" name="category" defaultValue={values.category} onBlur={handleBlur} onChange={handleChange}>
+                    <select className="form-select" name="categoryId" defaultValue={input.categoryId} onBlur={handleBlur} onChange={handleChange}>
                         <option hidden>Open this select menu</option>
-                        <option value="one">One</option>
+                        <option /* id of each category */ value="one">One</option>
                         <option value="two">Two</option>
                         <option value="three">Three</option>
                     </select>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Condition</label>
-                    <select className="form-select" name="condition" defaultValue={values.condition} onBlur={handleBlur} onChange={handleChange}>
+                    <select className="form-select" name="condition" defaultValue={input.condition} onBlur={handleBlur} onChange={handleChange}>
                         <option hidden>Open this select menu</option>
                         <option value="new">New</option>
                         <option value="used">Used</option>
                     </select>
                 </div>
                 <div className="mb-3">
-                    <label className="form-label">Amount</label>
-                    <input type="number" className="form-control" name="amount" defaultValue={values.amount} onBlur={handleBlur} onChange={handleChange} />
+                    <label className="form-label">Stock</label>
+                    <input type="number" className="form-control" name="stock" defaultValue={input.stock} onBlur={handleBlur} onChange={handleChange} />
                     <div className="form-text">Amount of products you have on stock</div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Default file input example</label>
-                    <input className="form-control" type="file" name="photos" defaultValue={values.photos} onBlur={handleBlur} onChange={handleChange} />
+                    <input className="form-control" type="file" name="images" defaultValue={input.images} onBlur={handleBlur} onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Description</label>
-                    <input type="text" className="form-control" name="description" defaultValue={values.description} onBlur={handleBlur} onChange={handleChange} />
+                    <input type="text" className="form-control" name="description" defaultValue={input.description} onBlur={handleBlur} onChange={handleChange} />
                     <div className="form-text">Give a brief description of your product</div>
                 </div>
                 <div className="input-group mb-3">
                     <span className="input-group-text">$</span>
-                    <input type="number" className="form-control" name="price" defaultValue={values.price} onBlur={handleBlur} onChange={handleChange} />
+                    <input type="number" className="form-control" name="price" defaultValue={input.price} onBlur={handleBlur} onChange={handleChange} />
                 </div>
-                <button className="btn btn-primary" onClick={this.continues}>Continue</button>
-                <button className="btn btn-light" onClick={this.back}>Back</button>
+                <button className="btn btn-primary" onClick={continues}>Continue</button>
+                <button className="btn btn-light" onClick={back}>Back</button>
             </form>
         )
     }
-}
+
+export default FormProductDescription
