04739ef0b10f2dfec0c28cf4274db1d13af78466
Jose Maria Marino Pereyra <87495251+xJomaMPx@users.noreply.github.com>
2022-01-31 12:14:32 -0300

    rutas para distintos casos de comprars

--- src/Views/Checkout/Checkout.js
+++ src/Views/Checkout/Checkout.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import styles from './_Checkout.module.scss';
+import CheckoutSuccess from "../../components/CheckoutSuccess/CheckoutSuccess";
+import CheckoutFailure from "../../components/CheckoutFailure/CheckoutFailure";
+import CheckoutPending from "../../components/CheckoutPending/CheckoutPending";
 
+import styles from "./_Checkout.module.scss";
+import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 
 
 const Checkout = () => {
-  return <div></div>;
+  const [timer,setTimer] = useState(1)
+
+
+  let {url} = useRouteMatch()
+  useEffect(()=>{
+  if(timer <= 4 ) {
+    setInterval(()=>{
+    setTimer(prevState => prevState = prevState + 1)
+  },1000)
+}},[timer])
+  
+
+  return (
+    <Switch>
+      <Route exact path={`${url}/success`}>
+        <CheckoutSuccess timer={timer}/>
+        {timer === 4 && <Redirect to="/"/>}
+      </Route>
+      <Route exact path={`${url}/failure`}>
+        <CheckoutFailure />
+        {timer === 4 && <Redirect to="/"/>}
+      </Route>
+      <Route exact path={`${url}/Pending`}>
+        <CheckoutPending />
+        {timer === 4 && <Redirect to="/"/>}
+      </Route>
+    </Switch>
+  );
 };
 
 export default Checkout;
